ee109827b8a0e5226fa5c06a88de03620667e881
zachdoug24 <30303429+zachdoug24@users.noreply.github.com>
2018-07-23 10:20:09 -0500

    Create supportsetup_cmd.js

---
+++ supportsetup_cmd.js
@@ -0,0 +1,13 @@
+{
+  "name": "supportsetup",
+  "permissions": "NONE",
+  "restriction": "4",
+  "_id": "NYMuy",
+  "actions": [
+    {
+      "source": "jaxBY",
+      "type": "false",
+      "name": "Call Command/Event"
+    }
+  ]
+}
